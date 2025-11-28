902346fe44061641bb6557421ace0570e5d19af0
YusukeYamasaki <91410662+Yusuke-Yamasaki-555@users.noreply.github.com>
2025-11-28 14:31:44 +0900

    main.py を更新中　全部のファイルのZを合わせる

--- ExpLogs/251124_1800/FlatStrike/1/main.py
+++ ExpLogs/251124_1800/FlatStrike/1/main.py
@@ -2,15 +2,60 @@ import csv
 from itertools import zip_longest
 
 def main():
-    file_title = "20251124204338"
-    file1_path = file_title+'_L.csv'
-    file2_path = file_title+'_R.csv'
+    file_title_1 = "20251124204338"
+    file_title_2 = "20251124204338"
+    file_title_3 = "20251124204338"
+    file_title_4 = "20251124204338"
+    file_title_5 = "20251124204338"
+    file_title_6 = "20251124204338"
+    file_title_7 = "20251124204338"
+    file_title_8 = "20251124204338"
+    file_title_9 = "20251124204338"
+    file_title_10 = "20251124204338"
+    file_path_1L = './1/'+file_title_1+'_L.csv'
+    file_path_1R = './1/'+file_title_1+'_R.csv'
+    file_path_2L = './2/'+file_title_2+'_L.csv'
+    file_path_2R = './2/'+file_title_2+'_R.csv'
+    file_path_3L = './3/'+file_title_3+'_L.csv'
+    file_path_3R = './3/'+file_title_3+'_R.csv'
+    file_path_4L = './4/'+file_title_4+'_L.csv'
+    file_path_4R = './4/'+file_title_4+'_R.csv'
+    file_path_5L = './5/'+file_title_5+'_L.csv'
+    file_path_5R = './5/'+file_title_5+'_R.csv'
+    file_path_6L = './6/'+file_title_6+'_L.csv'
+    file_path_6R = './6/'+file_title_6+'_R.csv'
+    file_path_7L = './7/'+file_title_7+'_L.csv'
+    file_path_7R = './7/'+file_title_7+'_R.csv'
+    file_path_8L = './8/'+file_title_8+'_L.csv'
+    file_path_8R = './8/'+file_title_8+'_R.csv'
+    file_path_9L = './9/'+file_title_9+'_L.csv'
+    file_path_9R = './9/'+file_title_9+'_R.csv'
+    file_path_10L = './10/'+file_title_10+'_L.csv'
+    file_path_10R = './10/'+file_title_10+'_R.csv'
     output_path = 'output.csv'
 
     try:
         # ファイルを開く (with文を使うことで自動的にcloseされます)
-        with open(file1_path, mode='r', encoding='utf-8', newline='') as f1, \
-             open(file2_path, mode='r', encoding='utf-8', newline='') as f2, \
+        with open(file_path_1L, mode='r', encoding='utf-8', newline='') as f1L, \
+             open(file_path_1R, mode='r', encoding='utf-8', newline='') as f1R, \
+             open(file_path_1L, mode='r', encoding='utf-8', newline='') as f1L, \
+             open(file_path_1R, mode='r', encoding='utf-8', newline='') as f1R, \
+             open(file_path_1L, mode='r', encoding='utf-8', newline='') as f1L, \
+             open(file_path_1R, mode='r', encoding='utf-8', newline='') as f1R, \
+             open(file_path_1L, mode='r', encoding='utf-8', newline='') as f1L, \
+             open(file_path_1R, mode='r', encoding='utf-8', newline='') as f1R, \
+             open(file_path_1L, mode='r', encoding='utf-8', newline='') as f1L, \
+             open(file_path_1R, mode='r', encoding='utf-8', newline='') as f1R, \
+             open(file_path_1L, mode='r', encoding='utf-8', newline='') as f1L, \
+             open(file_path_1R, mode='r', encoding='utf-8', newline='') as f1R, \
+             open(file_path_1L, mode='r', encoding='utf-8', newline='') as f1L, \
+             open(file_path_1R, mode='r', encoding='utf-8', newline='') as f1R, \
+             open(file_path_1L, mode='r', encoding='utf-8', newline='') as f1L, \
+             open(file_path_1R, mode='r', encoding='utf-8', newline='') as f1R, \
+             open(file_path_1L, mode='r', encoding='utf-8', newline='') as f1L, \
+             open(file_path_1R, mode='r', encoding='utf-8', newline='') as f1R, \
+             open(file_path_1L, mode='r', encoding='utf-8', newline='') as f1L, \
+             open(file_path_1R, mode='r', encoding='utf-8', newline='') as f1R, \
              open(output_path, mode='w', encoding='utf-8', newline='') as f_out:
 
             reader1 = csv.reader(f1)
